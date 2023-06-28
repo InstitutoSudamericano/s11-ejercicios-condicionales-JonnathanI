fc5960113c1b401859b33d689367cf1fd8f1b660
JonnathanI <129433201+JonnathanI@users.noreply.github.com>
2023-06-28 11:52:32 -0500

    Add files via upload

---
+++ Ejercicio 4/index.js
@@ -0,0 +1,9 @@
+let fistname = "Jonnathan";
+let subject = "Base Datos Relacional";
+like = false;
+
+if (like) {
+    alert("Hola mi nombre es: "+fistname+" estudiante de la materia de "+ subject +". "+"Esta Materia: "+"Me Gusta.");
+} else {
+    alert("Hola mi nombre es: "+fistname+" estudiante de la materia de "+ subject +". "+"Esta Materia: "+"No Me Gusta.");
+}
