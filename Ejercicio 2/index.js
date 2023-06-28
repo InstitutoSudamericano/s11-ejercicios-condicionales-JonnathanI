fe614f87a583ef6f90352df86efd14a6db04a402
JonnathanI <129433201+JonnathanI@users.noreply.github.com>
2023-06-28 11:52:11 -0500

    Add files via upload

---
+++ Ejercicio 2/index.js
@@ -0,0 +1,18 @@
+/* Crear Variables */
+let fistname ;
+let age;
+let isMarried;
+
+fistname = "Marco";
+age = 25;
+isMarried = true;
+typeof(fistname);
+typeof(age);
+typeof(isMarried);
+
+if (isMarried) {
+    alert("Hola mi nombre es: "+fistname+" tengo "+ age +" años "+"y mi estado civil es: "+"Casado");
+} else {
+    alert("Hola mi nombre es: "+fistname+" tengo "+ age +" años "+"y mi estado civil es: "+"Soltero");
+}
+
